35cd69d23b8460bfcf20c199adb3ca478b586dcc
Ezequiel Agustin Ibarra <ibarra.ezequiel01@gmail.com>
2021-04-14 16:00:08 -0300

    Update LabelBottomXL.js

--- src/components/home/LabelBottomXL.js
+++ src/components/home/LabelBottomXL.js
@@ -1,17 +1,17 @@
 
-function LabelBottomXL(){
-
+function LabelBottomXL(props){
+    const {countCartItems, totalPrice} = props;
     return(
         <div className="container-fluid group-vh-4">
             <div className="row ">
                 <div className="col-xs-12 col-md-5 labelbottom">
-                    <label>7 unidades</label>
+                    <label>{countCartItems} Unidades</label>
                 </div>
                 <div className="col-xs-12 col-md-4 labelbottom">
                     <label>Total</label>
                 </div>
                 <div className="col-xs-12 col-md-3 labelbottom">
-                    <label>$1234.56</label>
+                    <label>$ {totalPrice}</label>
                 </div>
             </div>
         </div>
