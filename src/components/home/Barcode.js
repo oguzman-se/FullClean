352df1a273c82d5a50e7e13cea088fda35c439aa
Ezequiel Agustin Ibarra <ibarra.ezequiel01@gmail.com>
2021-04-20 17:28:14 -0300

    Update Barcode.js

--- src/components/home/Barcode.js
+++ src/components/home/Barcode.js
@@ -1,11 +1,40 @@
-import React from 'react'
-import {useState, useEffect} from 'react'
-function Barcode(){
+import React from "react";
+import { useState } from "react";
+import { useHome } from '../../context/home-context'
+function Barcode() {
+    const [barcode, setBarcode] = useState([]);
+    const {onAdd, products} = useHome();
+    const onChange = (e) => {
+        setBarcode(e.target.value);
+    };
+    
+    const searchProduct = (barcode) => {
+        Object.keys(products).forEach(
+            function(key){
+                if(products[key].id===barcode){
+                    return products[key]
+                }
+            }
+        )
+    }
 
-    const [barcode, setBarcode] = useState([]); 
-    return(
-        <input className="barcode" ></input>
-    )
+    const onKeyPresed = (e) => {
+        if (e.key === "Enter") {
+            onAdd(searchProduct(barcode))
+        }
+    };
+
+    return (
+        <div>
+            <input
+            onKeyPress={onKeyPresed}
+            onChange={onChange}
+            className="barcode"
+            ></input>
+        </div>
+        
+        
+    );
 }
 
 export default Barcode;
