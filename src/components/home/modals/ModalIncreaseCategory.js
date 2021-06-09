a9ded602acc46c06a9fcdfaa39445087c77ee407
Martín Riesco <martinriesco16@gmail.com>
2021-06-09 15:52:12 -0300

    ajustando stock, margen, listado pedidos y filter

--- src/components/home/modals/ModalIncreaseCategory.js
+++ src/components/home/modals/ModalIncreaseCategory.js
@@ -1,56 +1,118 @@
-import React, {useState} from 'react'
-import {Modal} from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.css';
-import {useHome} from '../../../context/home-context'
-import Button from '../Button';
-import clienteAxios from '../../../config/clienteAxios'
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import { useHome } from "../../../context/home-context";
+import Button from "../Button";
+import clienteAxios from "../../../config/clienteAxios";
 import { useToasts } from "react-toast-notifications";
 
 function ModalIconos(props) {
-  const [margenCategory, setMargenCategory] = useState({})
-  const {showIconoCategory, setShowIconosCategory, currentCategoria} =props;
-  const { addToast } = useToasts();
-  const handleClose = () => {
-    setShowIconosCategory(false);  
-  }
-  const handleChange = e=> {
-    const {name, value} = e.target;
-    setMargenCategory({...margenCategory, [name]:value})
-    console.log(margenCategory);
-  }
-  return (
-    <>
-      <Modal
-        show={showIconoCategory}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="modal-tittle">{currentCategoria.nombre}</Modal.Title>
-        </Modal.Header> 
-        <Modal.Body>
-        <label>Margen de Categoria</label>
-        <input
-            type="text"
-            className="form-control custom-input"
-            placeholder=""
-            value={margenCategory.nombre}
-            onChange={handleChange} 
-        />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button className="modal-button-create" >
-            Si
-          </Button>
-          <Button className="modal-button-cancel" onClick={handleClose}>
-            No
-          </Button>
-          
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
+    const [margenCategory, setMargenCategory] = useState({
+        prodsOn: false,
+        aumento: 0,
+    });
+    const {
+        showIconoCategory,
+        setShowIconosCategory,
+        currentCategoria,
+    } = props;
+    const { addToast } = useToasts();
+    const handleClose = () => {
+        setShowIconosCategory(false);
+    };
+
+    const handleChange = (e) =>
+        e.target.type === "checkbox"
+            ? setMargenCategory({
+                  ...margenCategory,
+                  [e.target.name]: !margenCategory[e.target.name],
+              })
+            : setMargenCategory({
+                  ...margenCategory,
+                  [e.target.name]: e.target.value,
+              });
+
+    const handleSubmit = async () => {
+        await clienteAxios
+            .put(
+                `productos/actualizacionmasiva/${currentCategoria.id}`,
+                margenCategory
+            )
+            .then((res) => {
+                addToast("Actualización masiva exitosa!", {
+                    appearance: "success",
+                    autoDismiss: true,
+                });
+                setMargenCategory({ prodsOn: false, aumento: 0 });
+            })
+            .catch((err) => {
+                addToast("Actualización masiva fallida!", {
+                    appearance: "error",
+                    autoDismiss: true,
+                });
+            });
+    };
+
+    return (
+        <>
+            <Modal
+                show={showIconoCategory}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="modal-tittle">
+                        Aumento porcentual de costos de la categoría:{" "}
+                        {currentCategoria.nombre}
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <label>Ingrese el porcentaje</label>
+                    <input
+                        type="number"
+                        className="form-control custom-input"
+                        placeholder="Ingrese el porcentaje a aumentar"
+                        name="aumento"
+                        value={margenCategory.aumento}
+                        onChange={handleChange}
+                    />
+                    <label>Aumento de precios </label>
+                    <input
+                        type="checkbox"
+                        name="prodsOn"
+                        onChange={handleChange}
+                        defaultChecked={margenCategory.prodsOn}
+                        style={{
+                            WebkitAppearance: "auto",
+                            marginLeft: "10px",
+                            marginTop: "10px",
+                            fontFamily: "Gotham",
+                        }}
+                    />
+                    <p style={{ fontSize: 12, color: "grey" }}>
+                        (Si chequea esta opción, no solo se aumentan los costos
+                        de los productos si no tambien los precios finales)
+                    </p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button
+                        className="modal-button-create"
+                        onClick={handleSubmit}
+                    >
+                        CONTINUAR - Actualizar todos los productos de esta
+                        categoría
+                    </Button>
+                    <Button
+                        className="modal-button-cancel"
+                        onClick={handleClose}
+                    >
+                        CANCELAR
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
 }
 
 export default ModalIconos;
--- src/components/pedidos/ListPedidos.js
+++ src/components/pedidos/ListPedidos.js
@@ -1,71 +1,91 @@
-import React, {useState} from 'react'
-import clienteAxios from '../../config/clienteAxios'
-import {useHome} from '../../context/home-context'
-import Pendiente from './Pedidos'
-import {usePedidos} from '../../context/pedidos-context'
+import React, { useState } from "react";
+import clienteAxios from "../../config/clienteAxios";
+import { useHome } from "../../context/home-context";
+import Pendiente from "./Pedidos";
+import { usePedidos } from "../../context/pedidos-context";
 function ListPedidos(props) {
-    const {products, cartItems,setCartItems, Allclientes, setLabelCliente, setEnable, pendiente} = useHome()
-    const {setCurrentPedido, setHelpCurrentPedido} = usePedidos()
+    const {
+        products,
+        cartItems,
+        setCartItems,
+        Allclientes,
+        setLabelCliente,
+        setEnable,
+        pendiente,
+    } = useHome();
+    const { setCurrentPedido, setHelpCurrentPedido } = usePedidos();
     const [showPendiente, setShowPendiente] = useState(false);
-    const {setShowPedidosPendientes, showPedidosPendientes} = props;
-    const onSubmit = async(id)=>{
-        await clienteAxios.get(`/pedidodetalles/pedido/${id}`)
-        .then((res)=>{
-            let arr = res.data;
-            let ArrayFinal = []
-            arr.map((item)=> {
-                let dataProduct = products.filter((p)=> p.id === item.producto_id) 
-                if(dataProduct[0]){
-                    let productoIdeal = {
-                        id: dataProduct[0].id,
-                        nombre: dataProduct[0].nombre,
-                        precio: item.precio,
-                        qty: item.cantidad,
+    const { setShowPedidosPendientes, showPedidosPendientes } = props;
+    const onSubmit = async (id) => {
+        await clienteAxios
+            .get(`/pedidodetalles/pedido/${id}`)
+            .then((res) => {
+                let arr = res.data;
+                let ArrayFinal = [];
+                arr.map((item) => {
+                    let dataProduct = products.filter(
+                        (p) => p.id === item.producto_id
+                    );
+                    if (dataProduct[0]) {
+                        let productoIdeal = {
+                            id: dataProduct[0].id,
+                            nombre: dataProduct[0].nombre,
+                            precio: item.precio,
+                            qty: item.cantidad,
+                        };
+                        ArrayFinal.push(productoIdeal);
                     }
-                    ArrayFinal.push(productoIdeal)
-                }
-                return ""
+                    return "";
+                });
+                setCartItems(ArrayFinal);
+                setEnable(true);
             })
-            setCartItems(ArrayFinal)
-            setEnable(true)
-        })
-        .catch((e)=>{
-            console.log(e)
-        })
-    }
-    const masterSubmit = async(pedido)=>{
-        if(cartItems.length > 0 && pendiente === false){
-            setShowPendiente(true)
-            setHelpCurrentPedido(pedido)
-        }else{
-            setCurrentPedido(pedido)
-            let currentCliente ;
-            if (pedido.cliente_id !== 0){
-                currentCliente = Allclientes.filter((c)=> c.id === pedido.cliente_id)
-            } else{
-                currentCliente = [{
-                    id: 0,
-                    cliente: "Consumidor Final"
-                }]
+            .catch((e) => {
+                console.log(e);
+            });
+    };
+    const masterSubmit = async (pedido) => {
+        if (cartItems.length > 0 && pendiente === false) {
+            setShowPendiente(true);
+            setHelpCurrentPedido(pedido);
+        } else {
+            setCurrentPedido(pedido);
+            let currentCliente;
+            if (pedido.cliente_id !== 0) {
+                currentCliente = Allclientes.filter(
+                    (c) => c.id === pedido.cliente_id
+                );
+            } else {
+                currentCliente = [
+                    {
+                        id: 0,
+                        cliente: "Consumidor Final",
+                    },
+                ];
             }
-            setLabelCliente(currentCliente[0])
-            onSubmit(pedido.id)
-            if(showPedidosPendientes === true){
-                setShowPedidosPendientes(false)
+            setLabelCliente(currentCliente[0]);
+            onSubmit(pedido.id);
+            if (showPedidosPendientes === true) {
+                setShowPedidosPendientes(false);
             }
-            
         }
-    }
+    };
     //BUSCADOR DE PEDIDOS
-    const [buscarPedidos, setBuscarPedidos] = useState("");
-    const buscadorPedidos = (buscarPedidos) => {
-        return function(x){
-            console.log(x.ciente_id)
-        return (
-            x.estado.toLowerCase().includes(buscarPedidos) || !buscarPedidos
-        ) 
+    const [buscarPedidos, setBuscarPedidos] = useState({
+        text: "",
+        estado: "confirmado",
+    });
+    const buscadorPedidos = (pedido) => {
+        if (pedido.tipo_pedido === "venta") {
+            if (buscarPedidos.text !== "") {
+                if (pedido.id.toString().includes(buscarPedidos.text)) {
+                    return pedido;
+                }
+            } else {
+                return pedido;
+            }
         }
-    }
+    };
     return (
         <Pendiente
             buscarPedidos={buscarPedidos}
@@ -77,9 +97,8 @@ function ListPedidos(props) {
             onSubmit={onSubmit}
             showPedidosPendientes={showPedidosPendientes}
             setShowPedidosPendientes={setShowPedidosPendientes}
-            
         />
-    )
+    );
 }
 
 export default ListPedidos;
